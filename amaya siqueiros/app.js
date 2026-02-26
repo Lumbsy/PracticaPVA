b1acae7d23f648cebd8ff6e0272ae6135b3884e0
Lumbsy <angel.villegas10@icloud.com>
2026-02-26 07:12:41 -0800

    Add files via upload

---
+++ amaya siqueiros/app.js
@@ -0,0 +1,6 @@
+function MOSTRARMENSAJE(){
+    //imprime en consola
+    console.log("hola mundo desde la funcion. ")
+    //muestra en la pagina
+    document.getElementById("salida").innerHTML="¡Hola mundo desde Java";
+}
